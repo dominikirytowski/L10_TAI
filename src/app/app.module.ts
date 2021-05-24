5262f47489f73b88a998cb4556fbbf37e37f4569
Dominik <kamyka100@gmail.com>
2021-05-24 09:16:44 +0200

    done ex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,14 @@ import {FilterPipe} from './pipes/filter.pipe';
 import {TextFormatDirective} from './directives/text-format.directive';
 import {BlogDetailsComponent} from "./components/blog-details/blog-details.component";
 import { SelectizeComponent } from './components/selectize/selectize.component';
+import {AuthService} from "./services/auth.service";
+import {HTTP_INTERCEPTORS} from
+    '@angular/common/http';
+import {AuthInterceptor} from
+    './services/auth.interceptor';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+
 
 @NgModule({
   declarations: [
@@ -37,7 +45,9 @@ import { SelectizeComponent } from './components/selectize/selectize.component';
     BlogHomeComponent,
     FilterPipe,
     TextFormatDirective,
-    SelectizeComponent
+    SelectizeComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
@@ -46,7 +56,14 @@ import { SelectizeComponent } from './components/selectize/selectize.component';
     FormsModule
   ],
   providers: [
-    DataService
+    DataService,
+    AuthService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+
   ],
   bootstrap: [AppComponent]
 })
